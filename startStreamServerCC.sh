1b10d601e1aaabecb9bd822341c89a552e7f46f7
Roy Shadmon <16313057+royshadmon@users.noreply.github.com>
2022-06-06 07:20:16 -0700

    updating starting up screens

---
+++ startStreamServerCC.sh
@@ -0,0 +1,15 @@
+screen -S "Server0" -m -d runscripts/smartrun.sh bftsmart.demo.stream.StreamServer 0
+screen -S "Server1" -m -d runscripts/smartrun.sh bftsmart.demo.stream.StreamServer 1
+screen -S "Server2" -m -d runscripts/smartrun.sh bftsmart.demo.stream.StreamServer 2
+screen -S "Server3" -m -d runscripts/smartrun.sh bftsmart.demo.stream.StreamServer 3
+#screen -S "Server4" -m -d runscripts/smartrun.sh bftsmart.demo.stream.StreamServer 4
+#screen -S "Server5" -m -d runscripts/smartrun.sh bftsmart.demo.stream.StreamServer 5
+#screen -S "Server6" -m -d runscripts/smartrun.sh bftsmart.demo.stream.StreamServer 6
+#screen -S "Server7" -m -d runscripts/smartrun.sh bftsmart.demo.stream.StreamServer 7
+#screen -S "Server8" -m -d runscripts/smartrun.sh bftsmart.demo.stream.StreamServer 8
+#screen -S "Server9" -m -d runscripts/smartrun.sh bftsmart.demo.stream.StreamServer 9
+#screen -S "Server10" -m -d runscripts/smartrun.sh bftsmart.demo.stream.StreamServer 10
+#screen -S "Server11" -m -d runscripts/smartrun.sh bftsmart.demo.stream.StreamServer 11
+#screen -S "Server12" -m -d runscripts/smartrun.sh bftsmart.demo.stream.StreamServer 12
+
+# runscripts/smartrun.sh bftsmart.demo.stream.StreamClient 0 1 1000
