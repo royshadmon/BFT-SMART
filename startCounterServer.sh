5d85e5fe5a8def4c6afbafd342960a6c55645f37
Roy Shadmon <rshadmon@gmail.com>
2024-03-19 08:30:17 -0700

    CounterServer working for 3 layers

--- startCounterServer.sh
+++ startCounterServer.sh
@@ -3,29 +3,28 @@ count=0
 for (( id=0; id<$((num_replicas)); id++ ))
 do
 	echo "Starting layer 1 replica $id"
-#  	ttab -t 'R0' runscripts/smartrun.sh bftsmart.demo.counter.CounterServer $((id)) /Users/royshadmon/BFT-SMART/config/hosts.config /Users/royshadmon/BFT-SMART/src/main/java/bftsmart/demo/counter/layerConfigs/layer2.properties
   ttab -t 'R0' runscripts/smartrun.sh bftsmart.demo.counter.CounterServer $((id)) /Users/roy/Github-Repos/BFT-SMART/src/main/java/bftsmart/demo/counter/layerConfigs/layer1.properties
 	count=$((count+1))
 	echo $id
 
 
 done
-#count=0
-#max_replicas=$((num_replicas + count))
-#for (( id=$((count)); id<$((max_replicas)); id++ ))
-#do
-#	echo "Starting layer 1 replica $id"
-#  	ttab -t 'R0' runscripts/smartrun.sh bftsmart.demo.counter.CounterServer $((id)) /Users/roy/Github-Repos/BFT-SMART/src/main/java/bftsmart/demo/counter/layerConfigs/layer2.properties
-#	echo $id
-#
-#done
+count=0
+max_replicas=$((num_replicas + count))
+for (( id=$((count)); id<$((max_replicas)); id++ ))
+do
+	echo "Starting layer 2 replica $id"
+  	ttab -t 'R0' runscripts/smartrun.sh bftsmart.demo.counter.CounterServer $((id)) /Users/roy/Github-Repos/BFT-SMART/src/main/java/bftsmart/demo/counter/layerConfigs/layer2.properties
+	echo $id
+
+done
 
 count=0
 max_replicas=$((num_replicas + count))
 for (( id=$((count)); id<$((max_replicas)); id++ ))
 do
-	echo "Starting layer 1 replica $id"
-  	ttab -t 'R0' runscripts/smartrun.sh bftsmart.demo.counter.CounterServer $((id)) /Users/royshadmon/BFT-SMART/src/main/java/bftsmart/demo/counter/layerConfigs/layer3.properties
+	echo "Starting layer 3 replica $id"
+  	ttab -t 'R0' runscripts/smartrun.sh bftsmart.demo.counter.CounterServer $((id)) /Users/roy/Github-Repos/BFT-SMART/src/main/java/bftsmart/demo/counter/layerConfigs/layer3.properties
 	echo $id
 
 done
@@ -38,8 +37,8 @@ done
 
 
 
-ttab -t 'R0' runscripts/smartrun.sh bftsmart.demo.counter.CounterServer 0 /Users/roy/Github-Repos/BFT-SMART/src/main/java/bftsmart/demo/counter/layerConfigs/layer1.properties
-ttab -t 'R0' runscripts/smartrun.sh bftsmart.demo.counter.CounterServer 2 /Users/roy/Github-Repos/BFT-SMART/src/main/java/bftsmart/demo/counter/layerConfigs/layer1.properties
-ttab -t 'R0' runscripts/smartrun.sh bftsmart.demo.counter.CounterServer 3 /Users/roy/Github-Repos/BFT-SMART/src/main/java/bftsmart/demo/counter/layerConfigs/layer1.properties
-
-ttab -t 'R0' runscripts/smartrun.sh bftsmart.demo.counter.CounterServer 1 /Users/roy/Github-Repos/BFT-SMART/src/main/java/bftsmart/demo/counter/layerConfigs/layer1.properties
+#ttab -t 'R0' runscripts/smartrun.sh bftsmart.demo.counter.CounterServer 0 /Users/roy/Github-Repos/BFT-SMART/src/main/java/bftsmart/demo/counter/layerConfigs/layer1.properties
+#ttab -t 'R0' runscripts/smartrun.sh bftsmart.demo.counter.CounterServer 2 /Users/roy/Github-Repos/BFT-SMART/src/main/java/bftsmart/demo/counter/layerConfigs/layer1.properties
+#ttab -t 'R0' runscripts/smartrun.sh bftsmart.demo.counter.CounterServer 3 /Users/roy/Github-Repos/BFT-SMART/src/main/java/bftsmart/demo/counter/layerConfigs/layer1.properties
+#
+#ttab -t 'R0' runscripts/smartrun.sh bftsmart.demo.counter.CounterServer 1 /Users/roy/Github-Repos/BFT-SMART/src/main/java/bftsmart/demo/counter/layerConfigs/layer1.properties
